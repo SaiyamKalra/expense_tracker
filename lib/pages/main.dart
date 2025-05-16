236b4300fd3b177b9a477f740919db6af88e1bd6
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-16 20:23:01 +0530

    home page is ready

---
+++ lib/pages/main.dart
@@ -0,0 +1,22 @@
+import 'package:expense_tracker/cubit/expense_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import './home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
+  runApp(
+    BlocProvider(create: (context) => ExpenseCubit(), child: const MainApp()),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
